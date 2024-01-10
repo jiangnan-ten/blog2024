150a26969198f3cea49cf7bf5f0d5d62eafd75ae
jiangnan <tzhyten@gmail.com>
2024-01-10 13:22:44 +0800

    清空

--- docs/.vuepress/config.js
+++
@@ -1,8 +0,0 @@
-import { defineUserConfig } from 'vuepress'
-
-export default defineUserConfig({
-  base: '/blog2024/',
-  lang: 'zh-CN',
-  title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点',
-})
